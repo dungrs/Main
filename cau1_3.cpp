8deffadd3d3b3c0c8e64c163de83c9db6bc92465
dungrs <142152308+dungrs@users.noreply.github.com>
2024-05-17 14:53:42 +0700

    Update cau1_3.cpp

--- cau1_3.cpp
+++ cau1_3.cpp
@@ -22,157 +22,78 @@ struct HoaDon {
 };
 
 void NhapHangHoa() {
-    HangHoa hanghoa;
-    cout << "Nhap MaHH: ";
-    cin >> hanghoa.MaHH;
-    cout << "Nhap TenHH: ";
+    HangHoa h;
+    cout << "Nhap MaHH: "; cin >> h.MaHH;
     cin.ignore();
-    cin.getline(hanghoa.TenHH, 50);
-    cout << "Nhap NhaSX: ";
-    cin.getline(hanghoa.NhaSX, 50);
-    cout << "Nhap DonGia: ";
-    cin >> hanghoa.DonGia;
-
-    ofstream file("HangHoa.bin", ios::binary | ios::app);
-    if (!file.is_open()) {
-        cerr << "Loi! Khong the mo file HangHoa.bin" << endl;
-        return;
-    }
-    file.write((char*)&hanghoa, sizeof(HangHoa));
-    file.close();
+    cout << "Nhap TenHH: "; cin.getline(h.TenHH, 50);
+    cout << "Nhap NhaSX: "; cin.getline(h.NhaSX, 50);
+    cout << "Nhap DonGia: "; cin >> h.DonGia;
+    ofstream("HangHoa.bin", ios::binary | ios::app).write((char*)&h, sizeof(HangHoa));
     cout << "Hang hoa da duoc them vao file HangHoa.bin" << endl;
-    cout << "_____________________________________________" << endl;
 }
 
 void NhapHoaDon() {
-    HoaDon hoadon;
-    cout << "Nhap MaHH: ";
-    cin >> hoadon.MaHH;
-    cout << "Nhap MaKhach: ";
-    cin >> hoadon.MaKhach;
-    cout << "Nhap Soluong: ";
-    cin >> hoadon.Soluong;
-    cout << "Nhap Ngaymua: ";
-    cin >> hoadon.Ngaymua;
-    cout << "Nhap Thangmua: ";
-    cin >> hoadon.Thangmua;
+    HoaDon hd;
+    cout << "Nhap MaHH: "; cin >> hd.MaHH;
+    cout << "Nhap MaKhach: "; cin >> hd.MaKhach;
+    cout << "Nhap Soluong: "; cin >> hd.Soluong;
+    cout << "Nhap Ngaymua: "; cin >> hd.Ngaymua;
+    cout << "Nhap Thangmua: "; cin >> hd.Thangmua;
 
-    ifstream hanghoaFile("HangHoa.bin", ios::binary);
-    if (!hanghoaFile.is_open()) {
-        cerr << "Loi! Khong the mo file HangHoa.bin" << endl;
-        return;
-    }
-    HangHoa hanghoa;
-    int thanhtien = 0;
-    while (hanghoaFile.read((char*)&hanghoa, sizeof(HangHoa))) {
-        if (hanghoa.MaHH == hoadon.MaHH) {
-            thanhtien = hoadon.Soluong * hanghoa.DonGia;
-            break;
-        }
-    }
-    hanghoaFile.close();
-
-    hoadon.Thanhtien = thanhtien;
-
-    ofstream file("HoaDon.bin", ios::binary | ios::app);
-    if (!file.is_open()) {
-        cerr << "Loi! Khong the mo file HoaDon.bin" << endl;
-        return;
-    }
-    file.write((char*)&hoadon, sizeof(HoaDon));
+    ifstream file("HangHoa.bin", ios::binary);
+    HangHoa h;
+    while (file.read((char*)&h, sizeof(HangHoa)))
+        if (h.MaHH == hd.MaHH) { hd.Thanhtien = hd.Soluong * h.DonGia; break; }
     file.close();
+
+    ofstream("HoaDon.bin", ios::binary | ios::app).write((char*)&hd, sizeof(HoaDon));
     cout << "Hoa don da duoc them vao file HoaDon.bin" << endl;
-    cout << "_____________________________________________" << endl;
 }
 
 void TinhTongTien() {
     char MaKhach[6];
-    cout << "Nhap MaKhach: ";
-    cin >> MaKhach;
+    cout << "Nhap MaKhach: "; cin >> MaKhach;
 
-    ifstream hoadonFile("HoaDon.bin", ios::binary);
-    if (!hoadonFile.is_open()) {
-        cerr << "Loi! Khong the mo file HoaDon.bin" << endl;
-        return;
-    }
-    HoaDon hoadon;
-    HangHoa hanghoa;
-    int stt = 1;
-    int tongTien = 0;
+    ifstream hdFile("HoaDon.bin", ios::binary);
+    HoaDon hd;
+    HangHoa h;
+    int stt = 1, tongTien = 0;
 
-    // đoạn này hơi khó hiểu, sẽ giải thích sau!
-    ofstream khachHangFile((MaKhach + string(".txt")).c_str(), ios::app);
-    if (!khachHangFile.is_open()) {
-        cerr << "Loi! Khong the tao file cho khach hang " << MaKhach << endl;
-        return;
-    }
-
-    khachHangFile << "STT\tTenHH\t\t\tSoLuong\tDonGia\tThanhTien" << endl;
+    ofstream khachFile((string(MaKhach) + ".txt").c_str());
+    khachFile << "STT\tTenHH\t\t\tSoLuong\tDonGia\tThanhTien" << endl;
     cout << "STT\tTenHH\t\t\tSoLuong\tDonGia\tThanhTien" << endl;
-    while (hoadonFile.read((char*)&hoadon, sizeof(HoaDon))) {
-        // Hàm strcmp đươc sử dụng để so sánh 2 chuỗi kí tự
-        // Giá trị âm nếu chuỗi thứ nhất (str1) nhỏ hơn chuỗi thứ hai (str2).
-        // Giá trị dương nếu chuỗi thứ nhất (str1) lớn hơn chuỗi thứ hai (str2).
-        // Giá trị 0 nếu hai chuỗi bằng nhau.
-        if (strcmp(hoadon.MaKhach, MaKhach) == 0) {
-            ifstream hanghoaFile("HangHoa.bin", ios::binary);
-            if (!hanghoaFile.is_open()) {
-                cerr << "Loi! Khong the mo file HangHoa.bin" << endl;
-                return;
-            }
-            
-            while (hanghoaFile.read((char*)&hanghoa, sizeof(HangHoa))) {
-                if (hanghoa.MaHH == hoadon.MaHH) {
-                    cout << stt << "\t" << hanghoa.TenHH << "\t\t\t" << hoadon.Soluong << "\t" << hanghoa.DonGia << "\t" << hoadon.Thanhtien << endl;
-                    khachHangFile << stt << "\t" << hanghoa.TenHH << "\t\t\t" << hoadon.Soluong << "\t" << hanghoa.DonGia << "\t" << hoadon.Thanhtien << endl;
-                    tongTien += hoadon.Thanhtien;
+
+    while (hdFile.read((char*)&hd, sizeof(HoaDon)))
+        if (strcmp(hd.MaKhach, MaKhach) == 0) {
+            ifstream hFile("HangHoa.bin", ios::binary);
+            while (hFile.read((char*)&h, sizeof(HangHoa)))
+                if (h.MaHH == hd.MaHH) {
+                    cout << stt << "\t" << h.TenHH << "\t\t\t" << hd.Soluong << "\t" << h.DonGia << "\t" << hd.Thanhtien << endl;
+                    khachFile << stt << "\t" << h.TenHH << "\t\t\t" << hd.Soluong << "\t" << h.DonGia << "\t" << hd.Thanhtien << endl;
+                    tongTien += hd.Thanhtien;
                     break;
                 }
-            }
-            hanghoaFile.close();
+            hFile.close();
             stt++;
         }
-    }
-    khachHangFile << "Tong tien: " << tongTien << endl;
+    khachFile << "Tong tien: " << tongTien << endl;
     cout << "Tong tien: " << tongTien << endl;
-    
-    khachHangFile.close();
-    hoadonFile.close();
+    hdFile.close();
+    khachFile.close();
 }
-
 
 int main() {
     int luaChon;
-
     do {
-        // Hiển thị menu
-        cout << "\nMENU:" << endl;
-        cout << "1. Nhap hang hoa" << endl;
-        cout << "2. Nhap hoa don" << endl;
-        cout << "3. Tinh tong tien cho mot khach hang" << endl;
-        cout << "0. Thoat" << endl;
-        cout << "Lua chon cua ban: ";
+        cout << "\nMENU:\n1. Nhap hang hoa\n2. Nhap hoa don\n3. Tinh tong tien cho mot khach hang\n0. Thoat\nLua chon cua ban: ";
         cin >> luaChon;
-
-        // Xử lý các tùy chọn từ menu
         switch (luaChon) {
-            case 1:
-                NhapHangHoa();
-                break;
-            case 2:
-                NhapHoaDon();
-                break;
-            case 3:
-                TinhTongTien();
-                break;
-            case 0:
-                cout << "Tam biet!";
-                break;
-            default:
-                cout << "Lua chon khong hop le!";
-                break;
+            case 1: NhapHangHoa(); break;
+            case 2: NhapHoaDon(); break;
+            case 3: TinhTongTien(); break;
+            case 0: cout << "Tam biet!"; break;
+            default: cout << "Lua chon khong hop le!"; break;
         }
     } while (luaChon != 0);
-
     return 0;
 }
